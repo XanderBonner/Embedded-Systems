f50db4d3721df9b63ec6efff1a6bf5a1c8e6ae0d
Xander Bonner <157523819+XanderBonner@users.noreply.github.com>
2024-01-29 12:21:01 -0600

    Add files via upload

---
+++ Debug/SmallFont.d
@@ -0,0 +1,9 @@
+# FIXED
+
+SmallFont.o: ../SmallFont.c \
+ C:/Users/jossx/OneDrive/Desktop/319K\ Labs/MSPM0_ValvanoWare/ECE319K_Lab9/SmallFont.h \
+ C:/Users/jossx/OneDrive/Desktop/319K\ Labs/MSPM0_ValvanoWare/inc/ST7735.h
+
+C:/Users/jossx/OneDrive/Desktop/319K\ Labs/MSPM0_ValvanoWare/ECE319K_Lab9/SmallFont.h:
+
+C:/Users/jossx/OneDrive/Desktop/319K\ Labs/MSPM0_ValvanoWare/inc/ST7735.h:
